b958a54a9eeecbff32ebdb67bbae74f991c44096
Cloud Dark <syahdan.if16@student.unusa.ac.id>
2024-01-15 10:07:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,47 @@
     }, 1000);
   });
 })();
+// Assuming you have an HTML file with an element with id "card" to print the JSON content
+const cardDiv = document.getElementById('card');
+
+// Fetch the JSON file
+fetch('./config.json')
+  .then(response => response.json())
+  .then(data => {
+    // Access the required values from the JSON data
+    const namaAnda = data.nama_anda;
+    const namaPasangan = data.nama_pasangan;
+    const kalimat = data.kalimat;
+    
+    // Generate the HTML content
+    const htmlContent = `
+      <div id="card">
+        <div id="card-inside">
+          <div class="wrap">
+            <center>
+              <p class="animated infinite wobble"><b><u>Happy Birthday ${namaPasangan}</u></b></p>
+            </center>
+            <p> &nbsp;<i class="em-svg em-blush"></i></p>
+            <div id="text_kalimat">
+              ${kalimat}
+            </div>
+            <u>Made With <b><i class="em-svg em-heart"></i></b><br><b> By </b> <b>${namaAnda}</b></u>
+          </div>
+        </div>
+        <div id="card-front">
+          <div class="wrap">
+            <h1 class="animated infinite swing">Happy Birthday ${namaPasangan}</h1>
+            <div class="cake_and_velas">
+              <!-- Additional HTML content here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    // Print the HTML content to the cardDiv element
+    cardDiv.innerHTML = htmlContent;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
